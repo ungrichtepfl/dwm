ac18a38feb34945ab55886a8c2bbc167ca4bdbe9
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-02-04 22:15:17 +0100

    fix: No special rules
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- config.h
+++ config.h
@@ -21,14 +21,14 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+// NOTE: See https://dwm.suckless.org/customisation/rules/
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
