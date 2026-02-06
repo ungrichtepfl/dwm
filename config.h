8a3c00b25a2d33216639fec6f8495b65687a47ea
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-02-06 14:29:33 +0100

    feat: Colors and font.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- config.h
+++ config.h
@@ -11,27 +11,28 @@ static const int showsystray             =  1;        /* 0 means no systray */
 static const int showbar                 =  1;        /* 0 means no bar */
 static const int topbar                  =  1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "FontAwesomeNerdFont-12";
+static const char default_font[]    = "FontAwesomeNerdFont-12";
+static const char *fonts[]          = {default_font, "monospace:size=12" };
+#define					  dmenufont           default_font
 
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_dmenu_sf[]    = "#E5E9F0";
-static const char col_dmenu_sb[]    = "#586e75";
-static const char col_dmenu_nf[]    = "#9899a0";
-static const char col_dmenu_nb[]    = "#002b36";
+static const char col_gray2[]       = "#9899a0";
+static const char col_gray3[]       = "#586e75";
+static const char col_gray4[]       = "#E5E9F0";
+static const char col_cyan[]        = "#002b36";
+#define						col_dmenu_sf        col_gray4
+#define						col_dmenu_sb			  col_gray3
+#define						col_dmenu_nf			  col_gray2
+#define						col_dmenu_nb			  col_cyan 
 static const char *colors[][3]      = {
-	/*                   fg         bg         border   */
-	[SchemeNorm    ] = { col_gray3, col_gray1, col_gray2  },
-	[SchemeSel     ] = { col_gray4, col_cyan , col_cyan   },
-	[SchemeStatus  ] = { col_gray3, col_gray1, "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-  [SchemeTagsSel ] = { col_gray4, col_cyan , "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-  [SchemeTagsNorm] = { col_gray3, col_gray1, "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-  [SchemeInfoSel ] = { col_gray4, col_cyan , "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-  [SchemeInfoNorm] = { col_gray3, col_gray1, "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+	/*                   fg         bg          border   */
+	[SchemeNorm    ] = { col_gray3, col_cyan  , col_gray1  },
+	[SchemeSel     ] = { col_gray4, col_gray3 , col_gray1   },
+	[SchemeStatus  ] = { col_gray4, col_cyan  , "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+  [SchemeTagsSel ] = { col_gray4, col_gray3 , "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+  [SchemeTagsNorm] = { col_gray4, col_cyan  , "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+  [SchemeInfoSel ] = { col_gray4, col_gray3 , "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+  [SchemeInfoNorm] = { col_gray4, col_cyan  , "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
