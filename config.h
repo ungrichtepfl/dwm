906a33c28b29bd71434e04a928d50d86b0387235
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-02-08 17:31:32 +0100

    feat: change colorscheme
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- config.h
+++ config.h
@@ -35,14 +35,14 @@ static const char col_cyan[]        = "#002b36";
 static const char *colors[][3]      = {
 	/*                   fg         bg          border   */
 	[SchemeNorm    ] = { col_gray3, col_cyan  , col_gray1  },
-	[SchemeSel     ] = { col_gray4, col_gray3 , col_gray1   },
+	[SchemeSel     ] = { col_gray4, col_gray3 , col_gray1  },
 	[SchemeStatus  ] = { col_gray4, col_gray1 , "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
   [SchemeTagsSel ] = { col_gray4, col_gray3 , "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
   [SchemeTagsNorm] = { col_gray4, col_cyan  , "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-  [SchemeTabsSel ] = { col_gray4, col_cyan  , "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-  [SchemeTabsNorm] = { col_gray4, col_gray1 , "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-  [SchemeInfoSel ] = { col_gray4, col_gray3 , "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-  [SchemeInfoNorm] = { col_gray4, col_cyan  , "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+  [SchemeTabsSel ] = { col_gray4, col_gray3 , "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+  [SchemeTabsNorm] = { col_gray4, col_cyan  , "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+  [SchemeInfoSel ] = { col_gray4, col_gray1 , "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+  [SchemeInfoNorm] = { col_gray4, col_gray1 , "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
